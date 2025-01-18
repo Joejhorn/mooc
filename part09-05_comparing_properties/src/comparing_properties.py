2bd705834d66ccc42574e01cffab8c10e94048d4
Joe Horn <Joejhorn@yahoo.com>
2025-01-17 20:52:58 -0500

    updated new exercises

--- part09-05_comparing_properties/src/comparing_properties.py
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -1,7 +1,17 @@
 # Write your solution here:
-
 class RealProperty:
     def __init__(self, rooms: int , square_metres: int , price_per_sqm:int):
         self.rooms = rooms
         self.square_metres = square_metres
         self.price_per_sqm = price_per_sqm
+    
+    def bigger(self, other_property):
+        return self.square_metres > other_property.square_metres
+
+if __name__ == '__main__':
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    print(central_studio.bigger(downtown_two_bedroom))
+    print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
