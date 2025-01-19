71149a8217e55ec1062b6d111e77419be6f6a1dd
Joe Horn <Joejhorn@yahoo.com>
2025-01-19 17:41:32 -0500

    more work

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,31 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, name, account_number, balance):
+        self.__name = name
+        self.__account_number = account_number
+        self.__balance = balance
+
+    def __service_charge(self):
+        self.__balance -= self.__balance * .01
+
+    def deposit(self,amount):
+        self.__balance += amount
+        self.__service_charge()
+    
+    def withdraw(self, amount):
+        self.__balance -= amount
+        self.__service_charge()
 
 
+    
+    @property
+    def balance(self):
+        return self.__balance
+    
+
+if __name__ == '__main__':
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
