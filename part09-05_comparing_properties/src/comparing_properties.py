50a34b246b2fca334958f34c2122db43e44e3c10
Joe Horn <Joejhorn@yahoo.com>
2025-01-19 12:36:45 -0500

    just new stuff

--- part09-05_comparing_properties/src/comparing_properties.py
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -4,14 +4,22 @@ class RealProperty:
         self.rooms = rooms
         self.square_metres = square_metres
         self.price_per_sqm = price_per_sqm
+        self.total_price =  self.square_metres * self.price_per_sqm
     
     def bigger(self, other_property):
         return self.square_metres > other_property.square_metres
+    
+    def price_difference(self, other_property):
+        return abs(self.total_price - other_property.total_price)
+    
+    def more_expensive(self, other_property):
+        return self.total_price > other_property.total_price
+
 
 if __name__ == '__main__':
     central_studio = RealProperty(1, 16, 5500)
     downtown_two_bedroom = RealProperty(2, 38, 4200)
     suburbs_three_bedroom = RealProperty(3, 78, 2500)
 
-    print(central_studio.bigger(downtown_two_bedroom))
-    print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))
--- part09-08_shortest_in_room/src/shortest_in_room.py
+++ part09-08_shortest_in_room/src/shortest_in_room.py
@@ -6,3 +6,60 @@ class Person:
 
     def __str__(self):
         return self.name
+
+class Room:
+    def __init__(self):
+        self.persons_in_room = []
+        self.combined_height = 0
+
+    def add(self, person):
+        self.persons_in_room.append(person)
+        self.combined_height += person.height
+
+    def is_empty(self):
+        return (len(self.persons_in_room) == 0)
+    
+    def print_contents(self):
+        print (f'There are {len(self.persons_in_room)} persons in the room, and their combined height is {self.combined_height} cm')
+        for persons in self.persons_in_room:
+            print (f"{persons.name} ({persons.height} cm)")
+    
+    def shortest(self):
+        if len(self.persons_in_room) == 0:
+            return None
+        else:
+            shortest = self.persons_in_room[0]
+            for person in self.persons_in_room:
+                if person.height < shortest.height:
+                    shortest = person
+        return shortest
+    
+    def remove_shortest(self):
+        shortest = self.shortest()
+        if shortest is None:
+            return None
+        else:
+            self.persons_in_room.remove(shortest)
+            self.combined_height -= shortest.height
+        return shortest
+
+
+
+
+if __name__ == '__main__':
+    room = Room()
+
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Nina", 162))
+    room.add(Person("Ally", 166))
+    room.print_contents()
+
+    print()
+
+    removed = room.remove_shortest()
+    print(f"Removed from room: {removed.name}")
+
+    print()
+
+    room.print_contents()
