6ba726e145083e9d92ef5669ec519a2a36b6979c
Joe Horn <Joejhorn@yahoo.com>
2025-01-19 17:49:45 -0500

    blah

--- part09-10_recording/src/recording.py
+++ part09-10_recording/src/recording.py
@@ -1,18 +1,23 @@
 # WRITE YOUR SOLUTION HERE:
 class Recording:
     def __init__(self,length):
-        self.__length = length
-
+        if length > 0:
+            self.__length = length
+        else:
+            raise ValueError('Too short')
     @property
     def length(self):
         return self.__length
     
     @length.setter
     def length(self, length):
-        self.__length = length
+        if length > 0:
+            self.__length = length
+        else:
+            raise ValueError('Too short')
 
 if __name__ == '__main__':
-    the_wall = Recording(43)
+    the_wall = Recording(-1)
     print(the_wall.length)
     the_wall.length = 44
     print(the_wall.length)
