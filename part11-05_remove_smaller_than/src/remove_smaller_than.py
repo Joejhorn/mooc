8d05873ceaedf6b77ea9977b91be5f3994b1a33c
Joe Horn <Joejhorn@yahoo.com>
2025-02-11 15:54:20 -0500

    list comps, finally, fun

--- part11-05_remove_smaller_than/src/remove_smaller_than.py
+++ part11-05_remove_smaller_than/src/remove_smaller_than.py
@@ -1,1 +1,9 @@
 # WRITE YOUR SOLUTION HERE:
+def remove_smaller_than(number_list, limit):
+    return [number for number in number_list if number >= limit]
+
+
+if __name__ == '__main__':
+    numbers = [1,65, 32, -6, 9, 11]
+    print(remove_smaller_than(numbers, 10))
+    print(remove_smaller_than([-4, 7, 8, -100], 0))    
--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,17 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week_num, lottery_numbers):
+        self.week_num = week_num
+        self.lottery_numbers = lottery_numbers
+    
+    def number_of_hits(self, lottery_numbers):
+        return len([number for number in lottery_numbers if number in self.lottery_numbers])
+
+    def hits_in_place(self, numbers):
+        return [number if number in self.lottery_numbers else -1 for number in numbers]
+
+if __name__ == '__main__':
+    week8 = LotteryNumbers(8, [1,2,3,10,20,30,33])
+    my_numbers = [1,4,7,10,11,20,30]
+
+    print(week8.hits_in_place(my_numbers))
